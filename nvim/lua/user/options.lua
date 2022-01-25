fde3b03b3f7fb07bb93bf774c68ab4ac6f92c2fd
Corey Burrows <corey.burrows@gmail.com>
2022-01-25 14:07:53 -0600

    Add highlight columns.

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,7 +1,6 @@
 -- :help options
 vim.opt.backup = false                          -- creates a backup file
 vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
-vim.opt.cmdheight = 2                           -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
 vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
@@ -33,6 +32,7 @@ vim.opt.wrap = false                            -- display lines as one long lin
 vim.opt.scrolloff = 8                           -- is one of my fav
 vim.opt.sidescrolloff = 8
 vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
+vim.opt.colorcolumn="81,101"                    -- highlight the 81st, and 101st columns
 
 vim.opt.shortmess:append "c"
 
