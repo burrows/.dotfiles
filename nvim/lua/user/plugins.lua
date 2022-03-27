14c04c8b25b8dccc118f25d37ebfc36c8590871e
Corey Burrows <corey.burrows@gmail.com>
2022-03-27 18:43:53 -0500

    Add textsubjects plugin.

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -85,6 +85,7 @@ return packer.startup(function(use)
   use "nvim-treesitter/playground"
   use 'JoosepAlviste/nvim-ts-context-commentstring'
   use 'nvim-treesitter/nvim-treesitter-textobjects'
+  use 'RRethy/nvim-treesitter-textsubjects'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -63,5 +63,13 @@ configs.setup {
       },
     },
   },
-
+  textsubjects = {
+    enable = true,
+    prev_selection = ',', -- (Optional) keymap to select the previous selection
+    keymaps = {
+      ['.'] = 'textsubjects-smart',
+      [';'] = 'textsubjects-container-outer',
+      ['i;'] = 'textsubjects-container-inner',
+    },
+  },
 }
